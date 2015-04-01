7c9120343581c615914b94fdfa3e0931b8ad34da
yorktronic <ty.walker@gmail.com>
2015-04-01 16:36:50 -0700

    added comments

--- test-first-ruby/lib/12_rpn_calculator.rb
+++ test-first-ruby/lib/12_rpn_calculator.rb
@@ -55,7 +55,7 @@ class RPNCalculator
   		tokenized
   end
 
-  # Had to use Stack Overflow for help on this :( Essentially, what I was doing wrong was 1) not using Procs and then 2) not realizing that when I referenced the Procs in the methods hash, I need to do so using .call, i.e. methods[op].call. After re-reading Blocks and Procs in Learn to Program Chapter 10, this became obvious. It wasn't properly calling the methods (add, subtract, divide, etc otherwise). For some reason, on my computer at least, doign the has_key? test is faster by .003 seconds than checking if op is an integer, as in the solution on stack overflow
+  # Had to use Stack Overflow for help on this :( I knew I had to create a hash linking the symbols :+, :-, etc to method calls, but I was doing it wrong. Essentially, what I was doing wrong was 1) not using Procs when calling the arithmetic methods and then 2) not realizing that when I referenced the Procs in the methods hash, I need to do so using .call, i.e. methods[op].call. After re-reading Blocks and Procs in Learn to Program Chapter 10, this became obvious. It wasn't properly calling the methods (add, subtract, divide, etc otherwise). For some reason, on my computer at least, doign the has_key? test is faster by .003 seconds than checking if op is an integer. The solution on stack overflow checks if op is an integer, which is interestingly less efficient than my solution.
   def evaluate(ops)
   		ops = tokens(ops)
   		methods = {:+ => Proc.new{plus()}, :- => Proc.new{minus()}, :* => Proc.new{times()}, :/ => Proc.new{divide()}}
