6ca3f018b87667f2ede7f0881eb22ab1f1f61090
yorktronic <ty.walker@gmail.com>
2015-04-01 15:46:03 -0700

    Added comments to performance monitor

--- test-first-ruby/lib/06_performance_monitor.rb
+++ test-first-ruby/lib/06_performance_monitor.rb
@@ -1,3 +1,4 @@
+# I think I had to seek help online for this one - at the time I didn't fully understand block.call / yield so I had trouble figuring out some of the tests. I also got stuck at one point because I was doing something stupid like dividing the each run time by the number of runs, rather than dividing the total run time by number of runs in order to get the average.
 def measure(i = nil)
 	startTime = Time.now
 	run = 1
