089edad725ed665d3f9617a3ec7bf65180a21ebe
yorktronic <ty.walker@gmail.com>
2015-04-03 11:38:17 -0700

    Comments, code, etc
    Added comments. Updated title= to handle cases where the first word in a
    title is hyphenated, and the first sub-word is on the don't cap list.
    Added a cap? method to test if a word should be capitalized, cleaning up
    some of the code above it.

--- test-first-ruby/lib/08_book_titles.rb
+++ test-first-ruby/lib/08_book_titles.rb
@@ -1,18 +1,27 @@
+# A book class that lets you set the title of a book, and also titelize the title. Like in 03_simon_says.rb, the class utilizes a text file containing words to not capitalize (conjuctions, etc.) It also handles words with dashes and punctuation. I had to seek help online because I didn't understand how rpsec required a method to be called "title=", this syntax was unfamiliar to me. 
+#
+#
+
 class Book
 	$dont_cap = File.readlines("lib/dont_cap.txt").join.split(',')
 	
+	# When called, simply returns the book title - this method is only used when rspec needs to test the book title i.e. book.title.should == "To Kill a Mockingbird"
+	#
 	def title
 		@title 
 	end
 
+	# Sets the title of the book and properly titelizes it. This is the same code as in simon_says - later on I'll have simon_says require this file and use the method below
+	#
 	def title=(title)
 		words = title.split(" ")
 		
 		@title = words.map.with_index {|word, idx|
-			if !$dont_cap.include?(word) || idx == 0
-				#Handling titles with dashes
+			if cap?(word) || idx == 0
+				# If a word isn't in the don't cap list, we capitalize it, but first we need to see if it has a hyphen and test the sub-words in the hypenated word
+				#
 				if word.include?("-")
-					word.split("-").map! {|w| w.capitalize if !$dont_cap.include?(word) }.join("-")
+					word.split("-").map!.with_index {|w, i| w.capitalize if cap?(word) || i == 0  }.join("-")
 				else
 					word.capitalize
 				end
@@ -20,5 +29,11 @@ class Book
 				word
 			end
 		}.join(" ")
+	end
+
+	# A method that simply tests if a word should be capitalized, to clean up the code above
+	#
+	def cap?(word)
+		return true if !$dont_cap.include?(word)
 	end
 end
