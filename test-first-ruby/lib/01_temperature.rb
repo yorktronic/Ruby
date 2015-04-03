dbb12e37fdf2cd7c4eb628f42653b0cb1537839f
yorktronic <ty.walker@gmail.com>
2015-04-03 10:49:11 -0700

    Added comments

--- test-first-ruby/lib/01_temperature.rb
+++ test-first-ruby/lib/01_temperature.rb
@@ -1,8 +1,12 @@
+# Takes in a value in degrees fahrenheit and convertis it to celcius
+#
 def ftoc(degf)
 	return 0 if degf == 32
 	(degf - 32) * (5 / 9.0)
 end
 
+# Takes in a value in degrees celcius and converts it to fahrenheit
+#
 def ctof(degc)
 	return 32 if degc == 0
 	degc * (9 / 5.0) + 32
