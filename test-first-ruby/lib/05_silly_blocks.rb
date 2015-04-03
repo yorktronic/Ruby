01e0e1b06b89c4e40a697b8a0ad9e3d6404750c3
yorktronic <ty.walker@gmail.com>
2015-04-03 11:07:19 -0700

    Cleaned things up
    Used yield instead of block.call, cleaning things up a bit.

--- test-first-ruby/lib/05_silly_blocks.rb
+++ test-first-ruby/lib/05_silly_blocks.rb
@@ -1,11 +1,17 @@
-def reverser(&block)
+# Methods that do things to blocks that are passed in, like reversing strings, adding numbers, and repeating the block. I used yield here instead of block.call, because why not?
+#
+#
+
+# Reverses the string within the block that is called
+#
+def reverser()
 	yield.split(" ").map! {|word| word.reverse}.join(" ")
 end
 
-def adder(num = 1, &block)
+def adder(num = 1)
 	yield + num
 end
 
-def repeater(n = 1, &block)
+def repeater(n = 1)
 	n.times {yield}
 end
