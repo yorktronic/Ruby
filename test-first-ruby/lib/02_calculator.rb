54600ba062f015d27778b275605d3ddb4aeb3fa4
yorktronic <ty.walker@gmail.com>
2015-04-03 10:51:57 -0700

    Added comments

--- test-first-ruby/lib/02_calculator.rb
+++ test-first-ruby/lib/02_calculator.rb
@@ -1,25 +1,41 @@
+# Performs various calculator functions
+#
+#
+
+# Adds two numbers
+#
 def add(num1, num2)
 	num1 + num2
 end
 
+# Subtracts two numbers
+#
 def subtract(num1, num2)
 	num1 - num2
 end
 
+# Sums an array of numbers
+#
 def sum(nums)
 	return 0 if nums == []
 	nums.reduce(:+)
 end
 
+# Multiplies an array of numbers
+#
 def multiply(nums)
 	return 0 if nums == []
 	nums.reduce(:*)
 end
 
+# Takes a number to a given power (exponent)
+# 
 def power(num, power)
 	num**power
 end
 
+# Calculates the factorial of a number
+#
 def factorial(num)
 	(1..num).reduce(1,:*) 
 end
