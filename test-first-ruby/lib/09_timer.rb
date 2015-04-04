46925da8bc0b3c42264947cf2a036a7d7ac3a93c
yorktronic <ty.walker@gmail.com>
2015-04-04 09:39:14 -0700

    Added comments

--- test-first-ruby/lib/09_timer.rb
+++ test-first-ruby/lib/09_timer.rb
@@ -1,3 +1,6 @@
+# It's a Timer! Sort of... it basically just convert an integer of seconds in to the format of HH:MM:SS
+#
+#
 class Timer
 	
 	def initialize(seconds = 0)
@@ -12,6 +15,8 @@ class Timer
 		@seconds = seconds
 	end
 
+	# This method converts the integer to HH:MM:DD. I could simplify this to one line, doing all the calculations on the @time_string line, but this is way more legible
+	#
 	def time_string
 		hours = @seconds / 3600
 		minutes = (@seconds / 60) - (hours * 60)
@@ -19,6 +24,8 @@ class Timer
 		@time_string = "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
 	end
 
+	# A helper method that handles the case in which there is less than 10 seconds in a given digit (i.e. 5 hours should be 05 hours)
+	#
 	def padded(seconds)
 		return "0#{seconds.to_s}" if seconds < 10
 		seconds.to_s
