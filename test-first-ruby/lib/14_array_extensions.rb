c40f86d2c1841009640e8752396c6b887c77e23e
yorktronic <ty.walker@gmail.com>
2015-04-04 10:35:50 -0700

    Changed sum to use reduce instead of each
    Also added comments

--- test-first-ruby/lib/14_array_extensions.rb
+++ test-first-ruby/lib/14_array_extensions.rb
@@ -1,14 +1,20 @@
+# Extends the functionality of the Array class to include sum, square, and square!
+# 
 class Array
+	# Sums the values in an array
 	def sum
-		total = 0
-		self.each {|num| total += num}
-		total
+		return 0 if self == []
+		self.reduce(:+)
 	end
 
+	# Returns a new array containing all the values of the original array squared
+	#
 	def square
 		self.map {|num| num**2}
 	end
 
+	# Squares each element of the original array and returns it
+	#
 	def square!
 		self.map! {|num| num**2}
 	end
