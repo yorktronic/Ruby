3ddaea7dca9ee7cc01daf0d503d8eaed2485ada2
yorktronic <ty.walker@gmail.com>
2015-04-25 16:27:08 -0700

    finished 08_book_titles.rb

--- prep-work-master/test-first-ruby-master/lib/08_book_titles.rb
+++ prep-work-master/test-first-ruby-master/lib/08_book_titles.rb
@@ -1,3 +1,22 @@
 class Book
-  # TODO: your code goes here!
+  @title = ""
+  $dont_cap = File.readlines("lib/dont_cap.txt").join.split(',')
+ 
+  def title=(words)
+  		words = words.split(" ")
+  		@title = words.map!{|word| 
+  			if !$dont_cap.include?(word)
+  				word.capitalize
+  			else
+  				word
+  			end}
+  		@title[0].capitalize!
+  		@title = @title.join(" ")
+  end
+
+  def title
+  		@title
+  end
+
+
 end
