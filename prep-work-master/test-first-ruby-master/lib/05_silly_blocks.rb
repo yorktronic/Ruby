a544e39d6b9a94c6f7451764771351c4fc3af124
yorktronic <ty.walker@gmail.com>
2015-04-26 18:07:58 -0700

    asdf

--- prep-work-master/test-first-ruby-master/lib/05_silly_blocks.rb
+++ prep-work-master/test-first-ruby-master/lib/05_silly_blocks.rb
@@ -1,11 +1,11 @@
-def reverser (&block)
+def reverser ()
 	yield.split(" ").map! {|word| word.reverse}.join(" ")
 end
 
-def adder(add = 1, &block)
-	block.call + add
+def adder(add = 1)
+	yield + add
 end
 
-def repeater(n = 1, &block)
+def repeater(n = 1)
 	n.times {yield}
 end
